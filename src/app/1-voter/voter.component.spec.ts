f105a836316dc2c802bfb422421f968831e05bb6
srdjanRakic <srg_ian@hotmail.com>
2017-11-01 16:38:35 +0100

    Write integration tests for VoterComponent.

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,10 +1,37 @@
 import { VoterComponent } from './voter.component';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 describe('VoterComponent', () => {
+  let component: VoterComponent;
+  let fixture: ComponentFixture<VoterComponent>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ VoterComponent ]
+    });
+
+    fixture = TestBed.createComponent(VoterComponent);
+    component = fixture.componentInstance;
   });
 
-  it('', () => {
+  it('should render total votes', () => {
+    component.othersVote = 20;
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let debugElement = fixture.debugElement.query(By.css('.vote-count'));
+    let el: HTMLElement = debugElement.nativeElement;
+
+    expect(el.innerText).toContain('21');
+  });
+
+  it('should hightlight the upvote button if i have upvoted', () => {
+    component.myVote = 1;
+    fixture.detectChanges();
+
+    let debugElement = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+    expect(debugElement.classes['highlighted']).toBeTruthy();
   });
 });
